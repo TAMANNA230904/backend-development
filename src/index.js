a1003343dd84fcdbb4af959bd28a0837fbbff51d
TAMANNA <tamannasheikh2304@gmail.com>
2024-07-10 21:56:55 +0530

    adding more files

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!",err);
+})
 
 
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,33 @@
-const asynHandler=()=>{}
+const asynHandler=(reqHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(reqHandler(req,res,next)).
+        catch((err)=>next(err))
+    }
+}
 
 
 export {asyncHandler}
+
+
+
+
+
+
+
+
+
+//using try-catch
+// const asyncHandler=()=>{}
+// const asyncHandler=(fn)=>{}
+// const asyncHandler=(fn)=>{()=>{}}
+
+ //const asyncHandler=(fn)=>async (err,req,res,next)=>{
+ //  try{
+//   await fn(err,req,res,next)}
+//   catch(err)=>{
+//   resizeBy.status(err.code || 504).json({
+//     sucess: false,
+//     message:err.message
+//   })
+// }
+//}   
